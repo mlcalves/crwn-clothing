ecfa4c310bfc132063ee738f2d2c7a87f77b448b
Mario Alves <mlcalves@gmail.com>
2025-03-09 09:15:12 +0000

    import data change

--- src/importData.js
+++ src/importData.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Download this from Firebase Console
+const serviceAccount = require('../../Keys/serviceAccountKey.json'); // Download this from Firebase Console
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
